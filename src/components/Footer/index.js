5332461b4f1021f9df6a285236fb2b0a7573f127
193nkmr <109815945+193nkmr@users.noreply.github.com>
2024-09-08 22:42:02 +0900

    react-router-dom導入前まで

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            &copy; 2022 bluecode, inc. All Rights Reserved.
+        </footer>
+    )
+}
+export default Footer;
+
---
+++ src/screens/Top.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './styles/Top.css';
+
+const Top = () => {
+  return (
+    <>
+      <header className="top">
+        <h1 className="top_msg">test corporation website.</h1>
+      </header>
+      <main>
+        <section className="content">
+          <h2>test websiteの最新情報</h2>
+          <p>最新の情報をご案内します。</p>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default Top;
