0110e04221c4a166dc6de18c36533feca8ede19e
193nkmr <109815945+193nkmr@users.noreply.github.com>
2024-09-08 23:07:27 +0900

    react-hook-form導入とバリデーション実装

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,30 +1,63 @@
 import React from "react";
 import './styles/Contact.css';
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
+
+    const onSubmit = (data) => {
+
+        const api_url = 'apiのURL';
+
+        fetch(api_url, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: encodeURI(`name=${data.name}&email=${data.email}&body=${data.body}`)
+        })
+            .then((response) => response.json())
+            .then((result) => alert(result.message))
+            .catch((error) => alert(error.message))
+    }
     return (
         <main className="contact_form">
             <h2>お問合せ</h2>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="form_group">
                     <label>お名前</label>
-                    <input type="text" id="name" />
-                    <span className="form_error hidden" id="name_error">お名前は必須です。</span>
+                    <input type="text" id="name" {...register("name", { required: "お名前は必須です。" })} />
+                    {errors.name && <span className="form_error">{errors.name.message}</span>}
                 </div>
                 <div className="form_group">
                     <label>email</label>
-                    <input type="email" id="email" />
-                    <span className="form_error hidden" id="email_error">emailは必須かつemailの形式で入力してください。</span>
+                    <input type="email" id="email" {...register("email", {
+                        required: "emailは必須です。",
+                        pattern: {
+                            value: /^[a-z0-9.]+@[a-z]+\.[a-z]+$/,
+                            message: "emailの形式で入力してください。"
+                        }
+                    })} />
+                    {errors.email && <span className="form_error">{errors.email.message}</span>}
                 </div>
                 <div className="form_group">
                     <label>お問合せ内容</label>
-                    <textarea id="body" rows={4}></textarea>
-                    <span className="form_error hidden" id="body_error">お問合せ内容は必須かつ1文字以上10文字以下で入力してください。</span>
+                    <textarea id="body" rows={4} {...register("body", {
+                        required: "お問合わせ内容は必須です。",
+                        maxLength: {
+                            value: 10,
+                            message: "10文字以下で入力してください。"
+                        }
+                    })}></textarea>
+                    {errors.body && <span className="form_error">{errors.body.message}</span>}
                 </div>
                 <button className="submit_btn" id="submit_btn">送信</button>
             </form>
         </main >
     )
 }
-
 export default Contact;
