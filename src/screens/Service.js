f86a8bfe729c9bb67fd47c52a9fb0a78d9b188a8
193nkmr <109815945+193nkmr@users.noreply.github.com>
2024-09-12 00:18:04 +0900

    cardのリンク踏んだらどこかに飛ぶようにした

--- src/screens/Service.js
+++ src/screens/Service.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import Cardcomp from "../components/Cardcomp";
 import { Container, Box, Typography, Grid } from "@mui/material";
 
@@ -6,7 +7,7 @@ const card1 = {
     imgurl:'https://picsum.photos/1024/512?random=5',
     imgalt:'サービス１',
     title:'サービス１',
-    body:'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん'
+    body:'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんあいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんあいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんあいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん'
 }
 const card2 = {
     imgurl:'https://picsum.photos/1024/512?random=6',
@@ -36,7 +37,9 @@ const Service = () => {
                 <Box sx={{height: 20, width: "auto" }}></Box>
                 <Grid container spacing={2} sx={{ paddingTop: "10px", paddingBottom: "10px" }}>
                     <Grid item xs={6}>
-                        <Cardcomp {...card1} />
+                        <Link to="/AboutUs" style={{ textDecoration: 'none' }}>
+                            <Cardcomp {...card1} />
+                        </Link>
                     </Grid>
                     <Grid item xs={6}>
                         <Cardcomp {...card2} />
